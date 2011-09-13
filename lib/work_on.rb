919cc5b3c800bb141d3c29f9e0e00270b1a27398
Toon Willems <willemstoon@gmail.com>
2011-09-13 16:30:13 +0200

    Wrong directory structure

---
+++ lib/work_on.rb
@@ -0,0 +1,5 @@
+module WorkOn
+  autoload :Terminal , 'work_on/terminal.rb'
+  autoload :AbstractTerminal, 'work_on/terminal.rb'
+  autoload :MacTerminal, 'work_on/terminals/mac_terminal.rb'
+end
--- work_on.rb
+++
@@ -1,5 +0,0 @@
-module WorkOn
-  autoload :Terminal , 'lib/terminal.rb'
-  autoload :AbstractTerminal, 'lib/terminal.rb'
-  autoload :MacTerminal, 'lib/terminals/mac_terminal.rb'
-end
