d28f86425ff80f3043b6620248cc931b95b7b5f2
Toon Willems <willemstoon@gmail.com>
2011-09-19 14:38:12 +0200

    :Align , (or how i love vim)

--- lib/work_on.rb
+++ lib/work_on.rb
@@ -1,8 +1,9 @@
 module WorkOn
-  autoload :Terminal , 'work_on/terminal.rb'
-  autoload :AbstractTerminal, 'work_on/terminal.rb'
-  autoload :MacTerminal, 'work_on/terminals/mac_terminal.rb'
-  autoload :Config, 'work_on/config.rb'
-  autoload :Project, 'work_on/project.rb'
+  autoload :Terminal         , 'work_on/terminal.rb'
+  autoload :AbstractTerminal , 'work_on/terminal.rb'
+  autoload :MacTerminal      , 'work_on/terminals/mac_terminal.rb'
+  autoload :Config           , 'work_on/config.rb'
+  autoload :Project          , 'work_on/project.rb'
+  autoload :Cli              , 'work_on/cli.rb'
 
 end
