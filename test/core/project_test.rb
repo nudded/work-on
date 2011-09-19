697eda778c4a62020801e55065f285be7557b7f1
Toon Willems <willemstoon@gmail.com>
2011-09-19 09:55:47 +0200

    add Project test

---
+++ test/core/project_test.rb
@@ -0,0 +1,21 @@
+context "Project" do
+  helper(:config_hash) do
+    {'project-dir' => '~/test',
+     'tab1' => [],
+     'tab2' => []
+    }
+  end
+
+  setup { WorkOn::Project.new 'test', config_hash }
+
+  hookup do
+    terminal = stub(WorkOn::Terminal.instance)
+    # new_tab should be called twice
+    terminal.selected_window.mock!.new_tab.twice.stub!.execute
+    topic.work!
+  end
+
+
+  asserts_topic.assigns(:project_dir, '~/test')
+
+end
