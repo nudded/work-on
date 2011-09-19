d147cb736842fbf868169460092f564c6f8530c2
Toon Willems <willemstoon@gmail.com>
2011-09-19 14:37:41 +0200

    Use thor for cli

--- lib/work_on/cli.rb
+++ lib/work_on/cli.rb
@@ -1,13 +1,7 @@
+require 'thor'
+
 module WorkOn
-  class Cli
-    def self.start(*args)
-      first = args.shift
-      case first
-      when 'init'
-        # initialize with demo config file
-      else other
-        Config.default[first].work!
-      end
-    end
+  class Cli < Thor
+
   end
 end
