f5809510b57d10d53b6588b8a6fd13c557fe4465
Toon Willems <willemstoon@gmail.com>
2011-09-19 15:06:00 +0200

    basic implementation of Cli

--- lib/work_on/cli.rb
+++ lib/work_on/cli.rb
@@ -3,5 +3,14 @@ require 'thor'
 module WorkOn
   class Cli < Thor
 
+    desc "start PROJECT", "start working on the given project"
+    def start(project_name)
+      Config.default[project_name].work!
+    end
+
+    # use method_missing to start the project
+    def method_missing(name, *args)
+      start(name.to_s)
+    end
   end
 end
