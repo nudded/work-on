e9efa40abe9da4bfba9235a33a9e1e35971fea93
Toon Willems <willemstoon@gmail.com>
2011-09-20 17:11:35 +0200

    Project now uses Config class for its config
    This way you can have multiple config formats as long as they produce a
    sensible config object

--- lib/work_on/project.rb
+++ lib/work_on/project.rb
@@ -2,11 +2,13 @@ module WorkOn
 
   class Project
 
-    def initialize(name, config)
-      @name = name
+    def initialize(config)
       @config = config
-      @project_dir = @config['project-dir']
       @terminal = Terminal.instance
+    end
+
+    def project_dir
+      @config.project_dir
     end
 
     # Start working on this project
@@ -18,16 +20,13 @@ module WorkOn
 
     # Sets up the terminal according the config file
     def initialize_windows
-      windows = @config.select {|k,v| /window/ === k }
-      # if there is no window specified, i will assume the users wants to open the tabs in the current
-      # terminal window
-      if windows.empty?
-        tabs = @config.select {|k,v| /tab/ === k }
-        create_tabs(@terminal.selected_window, tabs, false)
-      else
-        windows.each do |window, tabs_hash|
-          create_window(tabs_hash)
-        end
+      # first create the tabs
+      tabs = @config.windows[:tabs]
+      create_tabs(@terminal.selected_window, tabs, false)
+
+      # then the windows
+      @config.windows[:windows].each do |tabs_hash|
+        create_window(tabs_hash[:tabs])
       end
     end
 
@@ -37,10 +36,10 @@ module WorkOn
       create_tabs(window, tabs)
     end
 
-    def create_tabs(window, hash, use_current = true)
+    def create_tabs(window, array, use_current = true)
       # create as many tabs as needed
       # if use_current is true, create one less and use the current one
-      count = hash.keys.size
+      count = array.size
       if use_current
         count -= 1
         first_tab = window.selected_tab
@@ -48,8 +47,8 @@ module WorkOn
       tabs = count.times.map { window.new_tab }
       tabs.unshift first_tab if use_current
 
-      hash.values.each.with_index do |commands, i|
-        tabs[i].execute "cd #{@project_dir}"
+      array.each.with_index do |commands, i|
+        tabs[i].execute "cd #{project_dir}"
         commands.each do |command|
           tabs[i].execute(command)
         end
